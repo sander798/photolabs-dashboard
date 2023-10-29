252e71632f769093053312e607fcea6137f5c266
Alexander Evans <sander7989@gmail.com>
2023-10-28 23:23:42 -0600

    add panels and state changing

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,21 +1,79 @@
 import React, { Component } from "react";
-import Loading from "components/Loading"
-
 import classnames from "classnames";
+import Loading from "components/Loading";
+import Panel from "components/Panel";
+
+const data = [
+  {
+    id: 1,
+    label: "Total Photos",
+    value: 10
+  },
+  {
+    id: 2,
+    label: "Total Topics",
+    value: 4
+  },
+  {
+    id: 3,
+    label: "User with the most uploads",
+    value: "Allison Saeng"
+  },
+  {
+    id: 4,
+    label: "User with the least uploads",
+    value: "Lukas Souza",
+  }
+];
+
 
 class Dashboard extends Component {
   state = {
     loading: false,
+    focused: null,
   };
 
+  selectPanel(id) {
+    this.setState(previousState => ({
+      focused: previousState.focused !== null ? null : id
+    }));
+  }
+
+  componentDidMount() {
+    const focused = JSON.parse(localStorage.getItem("focused"));
+
+    if (focused) {
+      this.setState({ focused });
+    }
+  }
+
+  componentDidUpdate(previousProps, previousState) {
+    if (previousState.focused !== this.state.focused) {
+      localStorage.setItem("focused", JSON.stringify(this.state.focused));
+    }
+  }
+
   render() {
-    const dashboardClasses = classnames("dashboard");
+    const dashboardClasses = classnames("dashboard", {
+      "dashboard--focused": this.state.focused
+    });     
 
     if (this.state.loading) {
       return <Loading />
     }
 
-    return <main className={dashboardClasses} />;
+    const panels = (this.state.focused ? data.filter(panel => this.state.focused === panel.id) : data)
+      .map(panel => (
+        <Panel
+          key={panel.id}
+          label={panel.label}
+          value={panel.value}
+          onClick={(event) => this.selectPanel(panel.id)}
+        />
+      ));
+
+
+    return <main className={dashboardClasses}>{panels}</main>;
   }
 }
 
